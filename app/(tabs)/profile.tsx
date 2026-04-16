a82d34983beab63c3eb8f836a2c582e3bb39d866
boyoung05 <boyoung05@kau.kr>
2026-04-16 16:08:47 +0900

    feat: 로그인 메인 화면 UI 구현

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,16 +1,5 @@
-import { Text, View } from "react-native";
+import ProfileView from "@/src/features/profile/screens/profilesScreen";
 
 export default function ProfileScreen() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: "#1a1a2e",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text style={{ color: "white", fontSize: 18 }}>프로필 페이지</Text>
-    </View>
-  );
+  return <ProfileView />
 }
