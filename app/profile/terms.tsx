0f222aba38ca0b9e1b9a97ac35ab74eb2ed6c9a0
boyoung05 <boyoung05@kau.kr>
2026-04-20 13:29:47 +0900

    feat: 개인정보처리방침 및 이용약관 노션 연결

---
+++ app/profile/terms.tsx
@@ -0,0 +1,5 @@
+import TermsScreen from "@/src/features/profile/screens/TermsScreen";
+
+export default function ProfileScreen() {
+    return <TermsScreen />
+}
---
+++ src/features/profile/data/profilePolicyLinks.ts
@@ -0,0 +1,6 @@
+export const profilePolicyLinks = {
+    /*이용약관*/ 
+    terms: "https://www.notion.so/34468f674e7d80b38fb5dd5bc7a5d3d5?p=34468f674e7d80579816fe64f3269a4b&pm=s",
+    /*개인정보처리방침*/
+    privacy: "https://www.notion.so/34468f674e7d80fb8084d92320c8b50e",
+};
