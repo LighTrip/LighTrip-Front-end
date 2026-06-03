b78a2ffe6dc1d9ab767bce01aa366600f86fa729
juminjae <minjae0812@naver.com>
2026-06-03 17:38:28 +0900

    feat: 접속 로그인 처리 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,34 @@
 // app/_layout.tsx
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Stack, useRouter } from "expo-router";
+import * as SecureStore from "expo-secure-store";
+import { useEffect } from "react";
 import "react-native-gesture-handler";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { useFonts } from 'expo-font';
 
 export default function RootLayout() {
+  const router = useRouter();
+
   const [fontsLoaded] = useFonts({
-    'Griun_Gellyroll': require('../assets/fonts/Griun_Gellyroll-Rg.ttf'),
-    'Moneygraphy': require('../assets/fonts/Moneygraphy-Pixel.ttf'),
-    'Freesentation': require('../assets/fonts/Freesentation-4Regular.ttf'),
+    Griun_Gellyroll: require("../assets/fonts/Griun_Gellyroll-Rg.ttf"),
+    Moneygraphy: require("../assets/fonts/Moneygraphy-Pixel.ttf"),
+    Freesentation: require("../assets/fonts/Freesentation-4Regular.ttf"),
   });
+
+  useEffect(() => {
+    if (!fontsLoaded) return;
+
+    const checkAuth = async () => {
+      const token = await SecureStore.getItemAsync("accessToken");
+      if (token) {
+        router.replace("/(tabs)");
+      } else {
+        router.replace("/(auth)");
+      }
+    };
+
+    checkAuth();
+  }, [fontsLoaded]);
 
   if (!fontsLoaded) {
     return null;
