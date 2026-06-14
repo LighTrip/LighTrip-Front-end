8ffa3693a4c998b55089d2fd3e0ee68c4bd43ebd
seoyeon ahn <ahnseoyn@gmail.com>
2026-06-14 18:03:54 +0900

    feat: 온디바이스 전처리 작업 구조 생성

--- src/features/passport/components/PassportList.tsx
+++ src/features/passport/components/PassportList.tsx
@@ -161,9 +161,9 @@ const PassportList = ({ onSelectPlace, initialTab = 'cover', onTabChange }: Prop
             </View>
 
             <SearchBar 
-            label={selectedDistrict ? `${selectedDistrict}구` : undefined} 
-            onSortChange={selected === 'cover' ? setCoverSortOrder : setListSortOrder}
-            onSearchChange={setSearchQuery}
+                label={selectedDistrict ? `${selectedDistrict}구` : undefined} 
+                onSortChange={selected === 'cover' ? setCoverSortOrder : setListSortOrder}
+                onSearchChange={setSearchQuery}
             />
 
             {selected === 'cover' ? coverContent : (
@@ -228,6 +228,7 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         gap: 11,
     },
+
 
     passportCover: {
         width: COVER_WIDTH,
